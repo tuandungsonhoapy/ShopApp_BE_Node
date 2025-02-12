c24d5e9b3854fc48d1a33eaa6980035742c35f4e
NgoPhuongDong-fithcmus <gonpro53@gmail.com>
2025-02-12 21:45:10 +0700

    continue products

--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -1,75 +1,3 @@
-// import Joi from 'joi'
-// import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators.js'
-
-// const PRODUCT_COLLECTION_NAME = 'products'
-// const PRODUCT_COLLECTION_SCHEMA = Joi.object({
-//   name: Joi.string().required().trim().strict(),
-//   description: Joi.string().optional().trim().strict(),
-//   price: Joi.number().required().min(0),
-//   thumbnail: Joi.string().optional().trim().strict().default(null),
-//   categoryId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
-
-//   createdAt: Joi.date().timestamp('javascript').default(Date.now()),
-//   updatedAt: Joi.date().timestamp('javascript').default(null),
-//   _destroy: Joi.boolean().default(false)
-// })
-
-// export const productModel = {
-//   PRODUCT_COLLECTION_NAME,
-//   PRODUCT_COLLECTION_SCHEMA
-// }
-
-// import mongoose from 'mongoose'
-// import { getDB } from '~/configs/mongodb.js'
-// import { OBJECT_ID_RULE } from '~/utils/validators.js'
-// import { handleThrowError } from '~/middlewares/errorHandlingMiddleware.js'
-
-// const PRODUCT_COLLECTION_NAME = 'products'
-
-// const PRODUCT_COLLECTION_SCHEMA = new mongoose.Schema(
-//   {
-//     title: { type: String, required: true, trim: true },
-//     product_category_id: { type: String, trim: true, default: '' },
-//     description: { type: String, trim: true },
-//     price: { type: Number, required: true, min: 0 },
-//     thumbnail: { type: String, trim: true, default: null },
-//     status: { type: String, trim: true },
-//     featured: { type: String, trim: true },
-//     slug: { type: String, trim: true },
-//     deleted: { type: Boolean, default: false },
-//     deletedTime: { type: Date, default: null },
-//     position: { type: Number },
-//     discountPercentage: { type: Number },
-//     stock: { type: Number, min: 0 },
-//     createdBy: {
-//       account_id: { type: String, required: true, match: OBJECT_ID_RULE },
-//       createdAt: { type: Date, default: Date.now }
-//     },
-//     deletedBy: {
-//       account_id: { type: String, match: OBJECT_ID_RULE },
-//       deletedAt: { type: Date, default: null }
-//     },
-//     updatedBy: [
-//       {
-//         account_id: { type: String, required: true, match: OBJECT_ID_RULE },
-//         updatedAt: { type: Date, default: null }
-//       }
-//     ]
-//   },
-//   { timestamps: true }
-// )
-
-// const Product = mongoose.model(PRODUCT_COLLECTION_NAME, PRODUCT_COLLECTION_SCHEMA)
-
-// const getAllProducts = async () => {
-//   return await getDB().collection(PRODUCT_COLLECTION_NAME).find()
-// }
-
-// export const productModel = { Product, PRODUCT_COLLECTION_NAME, PRODUCT_COLLECTION_SCHEMA, getAllProducts }
-//
-//
-//
-//
 // CODE MỚI
 // import mongoose from 'mongoose'
 import { ObjectId } from 'mongodb'
@@ -79,25 +7,6 @@ import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators.js'
 import { getDB } from '~/configs/mongodb.js'
 
 const PRODUCT_COLLECTION_NAME = 'products'
-
-// const PRODUCT_COLLECTION_SCHEMA = new mongoose.Schema(
-//   {
-//     title: { type: String, required: true, trim: true },
-//     product_category_id: { type: String, trim: true, default: '' },
-//     description: { type: String, trim: true },
-//     price: { type: Number, required: true, min: 0 },
-//     thumbnail: { type: String, trim: true, default: null },
-//     status: { type: String, trim: true },
-//     slug: { type: String, trim: true },
-//     deleted: { type: Boolean, default: false },
-//     stock: { type: Number, min: 0 },
-//     createdBy: {
-//       account_id: { type: String, required: true, match: OBJECT_ID_RULE },
-//       createdAt: { type: Date, default: Date.now }
-//     }
-//   },
-//   { timestamps: true }
-// )
 
 const PRODUCT_COLLECTION_SCHEMA = Joi.object({
   title: Joi.string().required(),
@@ -114,28 +23,20 @@ const PRODUCT_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false)
 })
 
-// const Product = mongoose.model(PRODUCT_COLLECTION_NAME, PRODUCT_COLLECTION_SCHEMA)
-
 const validateData = async (data: IProduct) => {
   return await PRODUCT_COLLECTION_SCHEMA.validateAsync(data, {
     abortEarly: false
   })
 }
 
-// // Tìm tất cả sản phẩm
-// const getAllProducts = async () => {
-//   return await PRODUCT_COLLECTION_SCHEMA.find({ deleted: false })
-// }
+// Tìm tất cả sản phẩm
 const getAllProducts = async () => {
   return await getDB().collection(PRODUCT_COLLECTION_NAME).find({ deleted: false }).toArray()
 }
 
-// // Tìm sản phẩm theo ID
-// const getProductById = async (id: string) => {
-//   return await PRODUCT_COLLECTION_SCHEMA.findOne({ _id: id, deleted: false })
-// }
+// Tìm sản phẩm theo ID
 const getProductById = async (id: string) => {
-  // if (!OBJECT_ID_RULE.test(id)) throw new Error(OBJECT_ID_RULE_MESSAGE)
+  if (!OBJECT_ID_RULE.test(id)) throw new Error(OBJECT_ID_RULE_MESSAGE)
   return await getDB()
     .collection(PRODUCT_COLLECTION_NAME)
     .findOne({ _id: new ObjectId(id), deleted: false })
@@ -149,9 +50,6 @@ const createProduct = async (data: IProduct) => {
 }
 
 // // Cập nhật sản phẩm
-// const updateProduct = async (id: string, updateData: IProduct) => {
-//   return await PRODUCT_COLLECTION_SCHEMA.findByIdAndUpdate(id, updateData, { new: true })
-// }
 const updateProduct = async (id: string, updateData: IProduct) => {
   if (!OBJECT_ID_RULE.test(id)) throw new Error(OBJECT_ID_RULE_MESSAGE)
   await validateData(updateData)
@@ -165,9 +63,6 @@ const updateProduct = async (id: string, updateData: IProduct) => {
 }
 
 // // Xóa mềm sản phẩm
-// const deleteProduct = async (id: string) => {
-//   return await PRODUCT_COLLECTION_SCHEMA.findByIdAndUpdate(id, { deleted: true }, { new: true })
-// }
 const deleteProduct = async (id: string) => {
   if (!OBJECT_ID_RULE.test(id)) throw new Error(OBJECT_ID_RULE_MESSAGE)
   return await getDB()
